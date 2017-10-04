75462179724b0f28255d56cf8516b7e2a0b74129
Chazona Baum <chznbaum@gmail.com>
2017-10-04 15:33:53 -0400

    implemented cors and faker

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+10.times do
+  FreelanceDocument.create!(
+    title: Faker::Lorem.words(rand(1..4)).join(' ').titleize,
+    description: Faker::Lorem.sentence,
+    file_url: Faker::Internet.url,
+    image_url: Faker::LoremPixel.image,
+    image_alt: Faker::Lorem.sentence,
+    client_id: rand(1..5),
+  )
+end
